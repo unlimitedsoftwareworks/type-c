2e25365a4f322b9ecc14fee1f4b6f5a18958b67d
praisethemoon <doit@praisethemoon.org>
2024-03-17 20:00:55 +0100

    fix attribute call

--- source/ast/expressions/FunctionCallExpression.ts
+++ source/ast/expressions/FunctionCallExpression.ts
@@ -62,50 +62,58 @@ export class FunctionCallExpression extends Expression {
                 let baseClass = baseExprType.to(ctx, ClassType) as ClassType;
 
                 /**
-                 * Since this is a function call, we expect the member to be a method,
-                 * we search for a method matching the signature creating using the given arguments and hint as return type (which might be null)
+                 * Check if it an attribute call, if it is not an attribute, it is a method call
+                 * if it is an attribute, we need to proceed with the regular logic
                  */
+                let isAttribute = baseClass.attributes.find(a => a.name === memberExpr.name);
+                if(isAttribute === undefined) {
 
-                let inferredArgTypes = this.args.map(e => e.infer(ctx, null));
-                let candidateMethods = baseClass.getMethodBySignature(ctx, memberExpr.name, inferredArgTypes, hint);
-                if(candidateMethods.length === 0) {
-                    throw ctx.parser.customError(`Method ${memberExpr.name} not found in class ${baseClass.shortname()}`, this.location);
-                }
-                if(candidateMethods.length > 1) {
-                    throw ctx.parser.customError(`Ambiguous method ${memberExpr.name} in class ${baseClass.shortname()}`, this.location);
-                }
+                    /**
+                     * Since this is a function call, we expect the member to be a method,
+                     * we search for a method matching the signature creating using the given arguments and hint as return type (which might be null)
+                     */
 
-                let method = candidateMethods[0];
-
-                /**
-                 * if we have a generic method, we need to infer the generic types
-                 */
-                if(method.generics.length !== memberExpr.typeArguments.length) {
-                    throw ctx.parser.customError(`Expected ${method.generics.length} type arguments, got ${memberExpr.typeArguments.length}`, this.location);
-                }
-
-                if(method.generics.length > 0) {
-                    let map = buildGenericsMaps(ctx, method.generics, memberExpr.typeArguments);
-                    let m = baseClass.getMethodByIndex(baseClass.getMethodIndexBySignature(ctx, memberExpr.name, inferredArgTypes, hint));
-                    if(m === null) {
-                        throw "unexpected null";
+                    let inferredArgTypes = this.args.map(e => e.infer(ctx, null));
+                    let candidateMethods = baseClass.getMethodBySignature(ctx, memberExpr.name, inferredArgTypes, hint);
+                    if(candidateMethods.length === 0) {
+                        throw ctx.parser.customError(`Method ${memberExpr.name} not found in class ${baseClass.shortname()}`, this.location);
+                    }
+                    if(candidateMethods.length > 1) {
+                        throw ctx.parser.customError(`Ambiguous method ${memberExpr.name} in class ${baseClass.shortname()}`, this.location);
                     }
 
-                    let newM = m.generateConcreteMethod(ctx, map, memberExpr.typeArguments);
-                    method = newM.imethod;
-                }
+                    let method = candidateMethods[0];
 
-                // since we might have a generic method, we need to re-infer the arguments of the call
-                for(let i = 0; i < this.args.length; i++){
-                    this.args[i].infer(ctx, method.header.parameters[i].type);
-                    if(!checkExpressionArgConst(this.args[i], method.header.parameters[i].type, method.header.parameters[i], method.header)) {
-                        throw ctx.parser.customError(`Argument ${i} is not assignable to parameter ${i}, mutability missmatch`, this.args[i].location);
+                    /**
+                     * if we have a generic method, we need to infer the generic types
+                     */
+                    if(method.generics.length !== memberExpr.typeArguments.length) {
+                        throw ctx.parser.customError(`Expected ${method.generics.length} type arguments, got ${memberExpr.typeArguments.length}`, this.location);
                     }
-                }
 
-                this.inferredType = method.header.returnType;
-                this.checkHint(ctx);
-                return this.inferredType;
+                    if(method.generics.length > 0) {
+                        let map = buildGenericsMaps(ctx, method.generics, memberExpr.typeArguments);
+                        let m = baseClass.getMethodByIndex(baseClass.getMethodIndexBySignature(ctx, memberExpr.name, inferredArgTypes, hint));
+                        if(m === null) {
+                            throw "unexpected null";
+                        }
+
+                        let newM = m.generateConcreteMethod(ctx, map, memberExpr.typeArguments);
+                        method = newM.imethod;
+                    }
+
+                    // since we might have a generic method, we need to re-infer the arguments of the call
+                    for(let i = 0; i < this.args.length; i++){
+                        this.args[i].infer(ctx, method.header.parameters[i].type);
+                        if(!checkExpressionArgConst(this.args[i], method.header.parameters[i].type, method.header.parameters[i], method.header)) {
+                            throw ctx.parser.customError(`Argument ${i} is not assignable to parameter ${i}, mutability missmatch`, this.args[i].location);
+                        }
+                    }
+
+                    this.inferredType = method.header.returnType;
+                    this.checkHint(ctx);
+                    return this.inferredType;
+                }
             }
             else if(baseExprType.is(ctx, InterfaceType)) {
                 let baseInterface = baseExprType.to(ctx, InterfaceType) as InterfaceType;
