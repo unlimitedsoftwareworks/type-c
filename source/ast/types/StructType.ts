3d5f86dc53cc40b8fef2fd58071e60f401e1f994
praisethemoon <doit@praisethemoon.org>
2024-09-07 13:50:50 +0200

    code gen helpers

--- source/ast/types/StructType.ts
+++ source/ast/types/StructType.ts
@@ -3,6 +3,7 @@ import {SymbolLocation} from "../symbol/SymbolLocation";
 import { Context } from "../symbol/Context";
 import { GenericType } from "./GenericType";
 import { getDataTypeByteSize } from "../../codegenerator/utils";
+import { matchDataTypes } from "../../typechecking/TypeChecking";
 
 export class StructField {
     name: string;
@@ -92,12 +93,52 @@ export class StructType extends DataType {
         }
     }
 
+    // CODE GEN API
+    
     getStructSize(ctx: Context){
         let sum: number = 0;
         for(let field of this.fields){
             sum += getDataTypeByteSize(field.type)
         }
         return sum;
+    }
+
+    fieldsAligned(struct: StructType, ctx: Context){
+        // check if the fields are aligned, i.e same order, same name
+        if(struct.fields.length != this.fields.length){
+            return false;
+        }
+        for(let i = 0; i < this.fields.length; i++){
+            if(this.fields[i].name != struct.fields[i].name){
+                return false;
+            }
+            if(!matchDataTypes(ctx, this.fields[i].type, struct.fields[i].type).success){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    generateOffsetSwaps(struct: StructType){
+        /**
+         * Generate new offsets for the struct fields, based on the order of the
+         * fields in the struct.
+         * for example {name: string, age: u32}.generateOffsetSwaps({age: u32, name: string})
+         * will return [1, 0], because the field "age" offset will be stored in the index 1 of 
+         * the offset array, and the field "name" offset will be stored in the index 0 of the
+         */
+
+        let offsets: number[] = [];
+        
+        for(let field of struct.fields){
+            let index = this.getFieldIndex(field.name);
+            if(index == -1){
+                throw new Error(`Field ${field.name} does not exist in struct ${this.shortname()}`);
+            }
+            offsets.push(index);
+        }
+
+        return offsets;
     }
 
 }
