2c49d3cd200106e01532c1d1b47f76937af9535b
praisethemoon <doit@praisethemoon.org>
2025-01-27 13:18:41 +0100

    fixed datatype reset

--- source/ast/types/DataType.ts
+++ source/ast/types/DataType.ts
@@ -90,6 +90,7 @@ export class DataType {
         DataType._recursiveGenericRecursion = [];
         DataType._recursiveTypeResolution = [];
         DataType._recursiveTypeSerialization = [];
+        DataType.ID_COUNTER = 0;
     }
 
     // the original type, i.e its definition reference
